1acdbde3ee1b30c201d33ccd267c5ffc98e4e956
Alexandru Muresan <102810131+AlexMuresanIT@users.noreply.github.com>
2025-10-06 21:37:01 +0300

    chore: refactor

--- src/main/java/health/tracker/api/schedulers/UserDailyRemindersScheduler.java
+++ src/main/java/health/tracker/api/schedulers/UserDailyRemindersScheduler.java
@@ -33,51 +33,38 @@ public class UserDailyRemindersScheduler {
       fixedDelayString = "${health-tracker.daily-reminders.drink-water.interval}",
       initialDelayString = "${health-tracker.daily-reminders.drink-water.initial-delay}")
   public void sendDrinkWaterReminder() {
-    if (healthTrackerConfig.getDailyReminders().enabled()) {
-      logger.info("Sending drink water reminders to all users");
-      final var allUsers = userService.getAllUsers();
-      allUsers.forEach(
-          user -> sendWhatsAppMessage(user.getPhoneNumber(), DRINK_WATER_DAILY_REMINDER_MESSAGE));
-    }
+    logger.info("Sending drink water reminders to all users");
+    sendReminder(DRINK_WATER_DAILY_REMINDER_MESSAGE);
   }
 
   @Scheduled(
       fixedDelayString = "${health-tracker.daily-reminders.stand-up.interval}",
       initialDelayString = "${health-tracker.daily-reminders.stand-up.initial-delay}")
   public void sendStandUpReminder() {
-    if (healthTrackerConfig.getDailyReminders().enabled()) {
-      logger.info("Sending stand up reminders to all users");
-      final var allUsers = userService.getAllUsers();
-      allUsers.forEach(
-          user -> sendWhatsAppMessage(user.getPhoneNumber(), STAND_UP_DAILY_REMINDER_MESSAGE));
-    }
+    logger.info("Sending stand up reminders to all users");
+    sendReminder(STAND_UP_DAILY_REMINDER_MESSAGE);
   }
 
   @Scheduled(
       fixedDelayString = "${health-tracker.daily-reminders.walk-reminder.interval}",
       initialDelayString = "${health-tracker.daily-reminders.walk-reminder.initial-delay}")
   public void sendWalkReminder() {
-    if (healthTrackerConfig.getDailyReminders().enabled()) {
-      logger.info("Sending walk reminders to all users");
-      final var allUsers = userService.getAllUsers();
-      allUsers.forEach(
-          user -> sendWhatsAppMessage(user.getPhoneNumber(), TAKE_A_WALK_DAILY_REMINDER_MESSAGE));
-    }
+    logger.info("Sending walk reminders to all users");
+    sendReminder(TAKE_A_WALK_DAILY_REMINDER_MESSAGE);
   }
 
   @Scheduled(
       fixedDelayString = "${health-tracker.daily-reminders.medication-reminder.interval}",
       initialDelayString = "${health-tracker.daily-reminders.medication-reminder.initial-delay}")
   public void sendMedicationReminder() {
-    if (healthTrackerConfig.getDailyReminders().enabled()) {
-      logger.info("Sending medication reminders to all users");
-      final var allUsers = userService.getAllUsers();
-      allUsers.forEach(
-          user -> sendWhatsAppMessage(user.getPhoneNumber(), MEDICATION_REMINDER_MESSAGE));
-    }
+    logger.info("Sending medication reminders to all users");
+    sendReminder(MEDICATION_REMINDER_MESSAGE);
   }
 
-  private void sendWhatsAppMessage(final String phoneNumber, final String message) {
-    whatsappApiService.sendWhatsappMessage(phoneNumber, message);
+  private void sendReminder(final String message) {
+    if (healthTrackerConfig.getDailyReminders().enabled()) {
+      final var users = userService.getAllUsers();
+      users.forEach(user -> whatsappApiService.sendWhatsappMessage(user.getPhoneNumber(), message));
+    }
   }
 }
